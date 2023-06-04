0525c39a9eeaa50fb19ba637df34b784917da0f7
Alexander Zaytsev <arkansaaaw@gmail.com>
2023-06-04 12:00:08 +0300

    Добавлена кнопка и лейбл с возможностью увеличивать значение при тапе

--- 3-rd Sprint - project/ViewController.swift
+++ 3-rd Sprint - project/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonCount: UIButton!
+    @IBOutlet weak var labelCount: UILabel!
 
+    var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelCount.text = "Значение счетчика: \n\(count)"
+        labelCount.numberOfLines = 4
+        labelCount.textAlignment = .center
     }
-
-
+    
+    @IBAction func incrementNumber(_ sender: Any) {
+        count += 1
+        labelCount.text = "Значение счетчика: \n\(count)"
+    }
 }
 
