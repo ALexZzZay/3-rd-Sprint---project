bec8bf1db9e725d8049cb2718c735947e4902660
Alexander Zaytsev <arkansaaaw@gmail.com>
2023-06-04 15:53:39 +0300

    Add minus, plus and reset button. Add autoscroll in the textField

--- 3-rd Sprint - project/ViewController.swift
+++ 3-rd Sprint - project/ViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
     
-    @IBOutlet weak var buttonCount: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
     @IBOutlet weak var labelCount: UILabel!
-
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var historyView: UITextView!
+    
     var count: Int = 0
+    var date: Date = Date()
+    let dateFormatter = DateFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +25,63 @@ class ViewController: UIViewController {
         labelCount.text = "Значение счетчика: \n\(count)"
         labelCount.numberOfLines = 4
         labelCount.textAlignment = .center
+        
+        historyView.textColor = .black
     }
     
     @IBAction func incrementNumber(_ sender: Any) {
         count += 1
+        
+    labelCount.text = "Значение счетчика: \n\(count)"
+    
+    dateFormatter.dateFormat = "dd.MM.yy - HH:mm"
+    var dateString = dateFormatter.string(from: date)
+    
+    historyView.text += "\n[\(dateString)]: Значение изменено на +1"
+    let range = NSMakeRange(historyView.text.count - 1, 0)
+    historyView.scrollRangeToVisible(range)
+        
+    }
+    
+    @IBAction func decrementNumber(_ sender: Any) {
+        if count <= 0 {
+            
+        count += 0
+            
+        dateFormatter.dateFormat = "dd.MM.yy - HH:mm"
+        var dateString = dateFormatter.string(from: date)
+        
+        historyView.text += "\n[\(dateString)]: Попытка уменьшить значение счётчика ниже 0"
+        let range = NSMakeRange(historyView.text.count - 1, 0)
+        historyView.scrollRangeToVisible(range)
+
+    } else
+        {
+            count -= 1
+        
+        dateFormatter.dateFormat = "dd.MM.yy - HH:mm"
+        var dateString = dateFormatter.string(from: date)
+        
+        historyView.text += "\n[\(dateString)]: Значение увеличилось на -1"
+        let range = NSMakeRange(historyView.text.count - 1, 0)
+        historyView.scrollRangeToVisible(range)
+        
+        }
+        
         labelCount.text = "Значение счетчика: \n\(count)"
+    }
+    
+    @IBAction func resetCount(_ sender: Any) {
+        count = 0
+        labelCount.text = "Значение счетчика: \n\(count)"
+        
+        dateFormatter.dateFormat = "dd.MM.yy - HH:mm"
+        var dateString = dateFormatter.string(from: date)
+        
+        historyView.text += "\n[\(dateString)]: Значение сброшено"
+        let range = NSMakeRange(historyView.text.count - 1, 0)
+        historyView.scrollRangeToVisible(range)
+        
     }
 }
 
